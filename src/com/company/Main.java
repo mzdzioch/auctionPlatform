d133c7cffb3195f0f0505cdf3c9040c2a290b56f
tblazejowski <tomasz.blazejowski@gmail.com>
2017-10-28 11:07:04 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Welcome to Allegro auction portal!");
+        System.out.println("type 1 if you want to log in\ntype 2 if you want to create new account");
+        Scanner input = new Scanner(System.in);
+        int firstLoginOption = input.nextInt();
+        switch (firstLoginOption){
+            case 1: {
+                System.out.println("enter you login: ");
+                String login = input.next();
+                System.out.println("enter your password: ");
+                String password = input.next();
+                User user = new User(login, password);
+                break;
+            }
+            case 2:{
+                System.out.println("enter you login: ");
+                String login = input.next();
+                System.out.println("enter your password: ");
+                String password = input.next();
+                User user = new User(login, password);
+                break;
+            }
+        }
+        input.close();
     }
 }
