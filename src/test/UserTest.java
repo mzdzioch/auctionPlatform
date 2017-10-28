a5f0089b7c305c362db89cc7bc659b5ebc2b86c7
ela-n <ela.nogiel@gmail.com>
2017-10-28 14:38:52 +0200

    class UserTest added

---
+++ src/test/UserTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import com.company.CredentialsToShortException;
+import com.company.LoginNullException;
+import com.company.User;
+import org.junit.Test;
+
+
+public class UserTest {
+
+
+    User testUser;
+
+
+
+    @Test(expected = LoginNullException.class)
+    public void shouldThrowExceptionWhenLoginIsNull() throws LoginNullException, CredentialsToShortException {
+        new User(null, "asdas");
+    }
+
+    @Test(expected = LoginNullException.class)
+    public void shouldThrowExceptionWhenPasswordIsNull() throws LoginNullException, CredentialsToShortException {
+        new User("fjkebejkbf", null);
+    }
+
+
+    @Test (expected = CredentialsToShortException.class)
+    public void shouldThrowExceptionWhenLoginIsTooShort() throws CredentialsToShortException, LoginNullException {
+        new User("fjke", "dsbjsbjbd");
+    }
+
+    @Test (expected = CredentialsToShortException.class)
+    public void shouldThrowExceptionWhenPasswordIsTooShort() throws CredentialsToShortException, LoginNullException {
+        new User("fjkeff", "dsbj");
+    }
+
+    
+
+}
+
