2a1abd8981610cd2a3d142e291f719907dd6cd82
mzdzioch <michal.zdziohc@gmail.com>
2017-10-28 18:22:36 +0200

    Tests for UserRegistry

---
+++ src/com/company/Category.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Category {
+
+    Set<TreeSet<Item>> parent = new TreeSet<>();
+
+    parent = CategoryBuilder.createCategory();
+
+
+
+
+
+}
---
+++ src/com/company/CategoryBuilder.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CategoryBuilder {
+
+
+
+    public static Set<TreeSet<Item>> getCategories(){
+        Set<TreeSet<Item>> parent = new TreeSet<>();
+        TreeSet<Item> node = new TreeSet<Item>();
+
+        return parent;
+    }
+}
