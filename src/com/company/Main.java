a2421f3d1bbb06fdcb224b47af5b57a7362d66af
tblazejowski <tomasz.blazejowski@gmail.com>
2017-10-28 12:38:53 +0200

    exceptions handled

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,11 @@
 package com.company;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws LoginExistException {
-        // write your code here
+    public static void main(String[] args) {
         System.out.println("Welcome to Allegro auction portal!");
         System.out.println("type [1] if you want to log in\n" +
                 "type [2] if you want to create new account\n" +
@@ -19,16 +19,27 @@ public class Main {
                     String login = input.next();
                     System.out.println("enter your password: ");
                     String password = input.next();
-                    User user = new User(login, password);
-                    UserRegistry userRegistry = new UserRegistry();
-                    if (userRegistry.existUser(user) == true) {
-                        System.out.println("Welcome " + user.login);
-                    } else {
-                        LoginExistException exception = new LoginExistException("login not found in database");
-                        System.out.println(exception.getMessage());
-                        System.out.println("type [2] if you want to create new account\n" +
-                                "type [0] if you want to exit\n");
-                        firstLoginOption = input.nextInt();
+                    User user = null;
+                    try {
+                        user = new User(login, password);
+                    } catch (CredentialsToShortException e) {
+                        e.printStackTrace();
+                    } catch (LoginNullException e) {
+                        e.printStackTrace();
+                    }
+                    UserRegistry userRegistry = new UserRegistry("users.txt");
+                    try {
+                        if (userRegistry.existUser(user) == true) {
+                            System.out.println("Welcome " + user.getLogin());
+                        } else {
+                            LoginExistException exception = new LoginExistException("login not found in database");
+                            System.out.println(exception.getMessage());
+                            System.out.println("type [2] if you want to create new account\n" +
+                                    "type [0] if you want to exit\n");
+                            firstLoginOption = input.nextInt();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
                     break;
                 }
@@ -37,7 +48,13 @@ public class Main {
                     String login = input.next();
                     System.out.println("create your password: ");
                     String password = input.next();
-                    User user = new User(login, password);
+                    try {
+                        User user = new User(login, password);
+                    } catch (CredentialsToShortException e) {
+                        e.printStackTrace();
+                    } catch (LoginNullException e) {
+                        e.printStackTrace();
+                    }
                     System.out.println("type [1] if you want to log in\n" +
                             "type [0] if you want to exit\n");
                     firstLoginOption = input.nextInt();
