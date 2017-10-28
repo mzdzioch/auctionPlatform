461cfe87cbe2c039c36d9c645ec5de1832f7d6e7
ela-n <ela.nogiel@gmail.com>
2017-10-28 10:54:11 +0200

    initial commit for User class

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -1,4 +1,29 @@
 package com.company;
 
 public class User {
+
+    String login;
+    String password;
+
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
