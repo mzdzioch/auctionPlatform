5c6ee837fe683c4465007433e3ba6582665cc0bb
mzdzioch <michal.zdziohc@gmail.com>
2017-10-29 10:02:31 +0100

    Updated Main Inteface

---
+++ src/com/company/Category.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Category {
+    private int id;
+    private String name;
+
+    public Category() {
+
+    }
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Node<Category> rootCategory = new Node<Category>(null, new Category());
+        Node<Category> electronicsCategory = new Node<Category>(rootCategory, new Category(1, "Elektronika"));
+        rootCategory.addChild(electronicsCategory);
+
+
         System.out.println("Welcome to Allegro auction portal!");
         System.out.println("type [1] if you want to log in\n" +
                 "type [2] if you want to create new account\n" +
                 "type [0] anytime you want to exit\n");
         Scanner input = new Scanner(System.in);
+        UserRegistry userRegistry = new UserRegistry("users.txt");
         int firstLoginOption = input.nextInt();
         do {
             switch (firstLoginOption) {
@@ -27,20 +33,23 @@ public class Main {
                     } catch (LoginNullException e) {
                         e.printStackTrace();
                     }
-                    UserRegistry userRegistry = new UserRegistry("users.txt");
                     try {
                         if (userRegistry.existUser(user) == true) {
                             System.out.println("Welcome " + user.getLogin());
+                            firstLoginOption = 0;
                         } else {
                             LoginExistException exception = new LoginExistException("login not found in database");
                             System.out.println(exception.getMessage());
-                            System.out.println("type [2] if you want to create new account\n" +
-                                    "type [0] if you want to exit\n");
-                            firstLoginOption = input.nextInt();
+//                            System.out.println("type [2] if you want to create new account\n" +
+//                                    "type [0] if you want to exit\n");
+//                            firstLoginOption = input.nextInt();
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                    System.out.println("type [2] if you want to create new account\n" +
+                                    "type [0] if you want to exit\n");
+                            firstLoginOption = input.nextInt();
                     break;
                 }
                 case 2: {
@@ -48,11 +57,17 @@ public class Main {
                     String login = input.next();
                     System.out.println("create your password: ");
                     String password = input.next();
+
                     try {
                         User user = new User(login, password);
+                        userRegistry.addUser(user);
                     } catch (CredentialsToShortException e) {
                         e.printStackTrace();
                     } catch (LoginNullException e) {
+                        e.printStackTrace();
+                    } catch (LoginExistException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
                     System.out.println("type [1] if you want to log in\n" +
