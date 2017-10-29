4f2a1220c84a3a74bb32ae1f5aea64ae323bfbdf
mzdzioch <michal.zdziohc@gmail.com>
2017-10-29 13:36:53 +0100

    Updated Main Inteface

--- src/com/company/CategoryBuilder.java
+++ src/com/company/CategoryBuilder.java
@@ -4,52 +4,92 @@ import java.util.*;
 
 public class CategoryBuilder {
 
-    private List<Node<String>> categories = new ArrayList<>();
+    private Node<Category> rootCategory;
+    private Map<Integer, Node<Category>> categoryMap;
 
-
-
-    public List<Node<String>> createCategory() {
-
-
-        Node<String> computers = new Node<>("Computers");
-        Node<String> cars = new Node<>("Cars");
-
-        Node<String> child1 = new Node<>("Servers");
-        child1.addChild("CPU");
-        child1.addChild("RAM");
-
-        Node<String> child2 = new Node<>("Desktops");
-        child2.addChild("Worstations");
-
-        Node<String> child3 = new Node<>("Laptops");
-        child2.addChild("Notbook");
-
-        computers.addChild(child1);
-        computers.addChild(child2);
-        computers.addChild(child3);
-
-
-        categories.add(computers);
-        categories.add(cars);
-
-        return categories;
+    public CategoryBuilder() {
+        this.rootCategory = createCategory();
     }
 
-    public void displayCategory() {
+    private Node<Category> createCategory() {
 
-        List<Node<String>> categories = createCategory();
+        rootCategory = new Node<Category>(null, new Category());
+        Node<Category> electronicCategory = new Node<Category>(new Category(1, "Electronics"));
+        Node<Category> motorsCategory = new Node<Category>(new Category(2, "Motors"));
+        Node<Category> clothesCategory = new Node<Category>(new Category(3, "Clothes"));
 
-        for (Node<String> category : categories) {
-            System.out.println("-" + category.getItem());
+        rootCategory.addChild(electronicCategory);
+        rootCategory.addChild(motorsCategory);
+        rootCategory.addChild(clothesCategory);
 
-            for (Node node : category.getChildren()) {
-                System.out.println("--" + node.getItem());
-                List<Node> nList = node.getChildren();
-                for (Node node1 : nList) {
-                    System.out.println("---" + node1.getItem());
-                }
-            }
+        Node<Category> laptopsCategory = new Node<Category>(new Category(12, "Laptops"));
+        Node<Category> pcCategory = new Node<Category>(new Category(13, "PC"));
+        Node<Category> serversCategory = new Node<Category>(new Category(14, "Servers"));
+
+        electronicCategory.addChild(laptopsCategory);
+        electronicCategory.addChild(pcCategory);
+        electronicCategory.addChild(serversCategory);
+
+        Node<Category> partsCategory = new Node<Category>(new Category(21, "Parts"));
+        Node<Category> carsCategory = new Node<Category>(new Category(22, "Cars"));
+        Node<Category> vehiclesCategory = new Node<Category>(new Category(23, "Vehicles"));
+        Node<Category> trucksCategory = new Node<Category>(new Category(24, "Trucks"));
+
+        motorsCategory.addChild(partsCategory);
+        motorsCategory.addChild(carsCategory);
+        motorsCategory.addChild(vehiclesCategory);
+        motorsCategory.addChild(trucksCategory);
+
+        Node<Category> womenClothingCategory = new Node<Category>(new Category(31, "Women Clothing"));
+        Node<Category> menClothingCategory = new Node<Category>(new Category(32, "Men Clothing"));
+        Node<Category> shoesCategory = new Node<Category>(new Category(33, "Shoes"));
+
+        clothesCategory.addChild(womenClothingCategory);
+        clothesCategory.addChild(menClothingCategory);
+        clothesCategory.addChild(shoesCategory);
+
+        return rootCategory;
+    }
+
+    public Node<Category> getBuilder(){
+        return rootCategory;
+    }
+
+    public void addRootCategory(int id, String name) {
+        if(rootCategory.isRoot())
+            rootCategory.addChild(new Category(id, name));
+        else {
+            System.out.println("Jestesmy ");
+            rootCategory = new Node<Category>(null, new Category());
+            rootCategory.addChild(new Category(id, name));
         }
 
     }
+
+    public void addSubCategory(int parentId, int id, String name) {
+        Node<Category> parent = findParentById(parentId);
+        parent.addChild(new Node<Category>(new Category(id, name)));
+    }
+
+    private Node<Category> findParentById(Integer parentId){
+        categoryMap = mapOfCategory();
+
+        return categoryMap.get(parentId);
+    }
+
+    private Map<Integer, Node<Category>> mapOfCategory() {
+
+        List<Node<Category>> nodeList = rootCategory.getChildren();
+
+        if(!nodeList.isEmpty()) {
+            for (Node<Category> categoryNode : nodeList) {
+                categoryMap.put(categoryNode.getItem().getId(), categoryNode);
+            }
+        }
+
+        return categoryMap;
+    }
+
+
+
 }
