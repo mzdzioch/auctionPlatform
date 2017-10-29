b6d465f8c1872c11e32133b9e90b0b82ce9f3984
tblazejowski <tomasz.blazejowski@gmail.com>
2017-10-29 11:19:24 +0100

    readAuctionsRegistryToMemory method added

--- src/com/company/AuctionRegistry.java
+++ src/com/company/AuctionRegistry.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,11 +9,58 @@ public class AuctionRegistry {
     private Map<Integer, Auction> listOfAuctions = new HashMap<Integer, Auction>();
     private String fileName;
 
-    public AuctionRegistry(Map<Integer, Auction> listOfAuctions) {
-        this.listOfAuctions = listOfAuctions;
+    public AuctionRegistry(String fileName) {
+        this.fileName = fileName;
         File file = new File(fileName);
         if (!file.exists()) {
             createAuctionsFile(fileName);
+        } else {
+            readAuctionsRegistryToMemory(fileName);
+        }
+    }
+
+    private void readAuctionsRegistryToMemory(String fileAuctionsName) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(fileAuctionsName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            int auctionID;
+//            int counter;
+            String title;
+            double price;
+            int categoryID;
+            String description;
+            String login;
+
+            String line = br.readLine();
+            String[] auctionToArray;
+
+            while (line != null) {
+                auctionToArray = line.split("|");
+                auctionID = Integer.parseInt(auctionToArray[0]);
+//            int counter;
+                title = auctionToArray[1];
+                price = Double.parseDouble(auctionToArray[2]);
+                categoryID = Integer.parseInt(auctionToArray[3]);
+                ;
+                description = auctionToArray[4];
+                login = auctionToArray[5];
+                Auction auction = new Auction(auctionID, title, price, categoryID, description, login);
+                listOfAuctions.put(auctionID, auction);
+                line = br.readLine();
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } finally {
+
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -38,6 +82,7 @@ public class AuctionRegistry {
             }
         }
     }
+
     public void writeAuction(Auction auction) {
 
         BufferedWriter bw = null;
@@ -47,20 +92,21 @@ public class AuctionRegistry {
             fw = new FileWriter(fileName, true);
             bw = new BufferedWriter(fw);
             bw.write(auction.getAuctionID() + "|"
+//                    + auction.getCounter() + "|"
                     + auction.getTitle() + "|"
                     + auction.getPrice() + "|"
                     + auction.getCategoryID() + "|"
                     + auction.getDescription() + "|"
-//                    + auction.getLogin() +
+                    + auction.getLogin()
                     + "\n");
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
-                if (bw !=null)
+                if (bw != null)
                     bw.close();
 
-                if (fw !=null)
+                if (fw != null)
                     fw.close();
 
             } catch (IOException ex) {
